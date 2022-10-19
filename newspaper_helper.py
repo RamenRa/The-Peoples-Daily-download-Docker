63cf272e9f44d61ae6706acd6773cd320bcace4b
liangjin.bai <liangjin.bai@megatronix.co>
2022-10-19 17:03:45 +0800

    refactor add param start date and end date

--- newspaper_helper.py
+++ newspaper_helper.py
@@ -4,7 +4,6 @@ import argparse
 import datetime
 import calendar
 import shutil
-import urllib
 
 import pytz
 import requests
@@ -25,18 +24,24 @@ def get_input_arg():
     # 解析输入参数
     parser = argparse.ArgumentParser(description='Manual to this script')
     parser.add_argument("--date", type=str, default=None, help='input date,format: 20220901')
+    parser.add_argument("--start_date", type=str, default=None, help='input start_date,format: 20220901')
+    parser.add_argument("--end_date", type=str, default=None, help='input end_date,format: 20220901')
     return parser.parse_args()
+
+
+def param_is_none(param):
+    return param is None or "none" == param
 
 
 def get_today_date():
     return datetime.datetime.now(timezone).strftime("%Y%m%d")
 
 
-def get_monday_date(date_str):
-    current_day = datetime.datetime.strptime(date_str, '%Y%m%d')
+def get_last_weekday_date(date, weekday):
+    current_day = datetime.datetime.strptime(date, '%Y%m%d')
     oneday = datetime.timedelta(days=1)
 
-    while current_day.weekday() != calendar.MONDAY:
+    while current_day.weekday() != weekday:
         current_day -= oneday
 
     return current_day.strftime('%Y%m%d')
@@ -50,6 +55,17 @@ def get_this_monday_date():
     return current_day.strftime('%Y%m%d')
 
 
+def get_date_list(start, end, week):
+    date_list = []
+    date = datetime.datetime.strptime(start, '%Y%m%d')
+    end = datetime.datetime.strptime(end, '%Y%m%d')
+    while date <= end:
+        if date.weekday() == week:
+            date_list.append(date.strftime('%Y%m%d'))
+        date = date + datetime.timedelta(1)
+    return date_list
+
+
 def get_date_list(start, end):
     date_list = []
     date = datetime.datetime.strptime(start, '%Y%m%d')
@@ -58,6 +74,12 @@ def get_date_list(start, end):
         date_list.append(date.strftime('%Y%m%d'))
         date = date + datetime.timedelta(1)
     return date_list
+
+
+def date_is_before(date_str, standard_date_str):
+    date = datetime.datetime.strptime(date_str, '%Y%m%d')
+    standard_date = datetime.datetime.strptime(standard_date_str, '%Y%m%d')
+    return date < standard_date
 
 
 def init_or_clear_dir(dir):
@@ -81,24 +103,34 @@ def creat_folder_if_not_exist(folder):
         os.makedirs(folder)
 
 
-def check_newspaper_exist(local_save_folder, remote_web_url, newspaper_filename):
+def check_local_newspaper_exist(local_save_folder, newspaper_filename):
     creat_folder_if_not_exist(local_save_folder)
 
-    if os.path.exists(local_save_folder):
-        filename_list = os.listdir(local_save_folder)
+    if not os.path.exists(local_save_folder):
+        return False
+    filename_list = os.listdir(local_save_folder)
+    return newspaper_filename in filename_list
 
-        if newspaper_filename in filename_list:
-            print("该日期已经下载过了!")
-            exit(0)
 
-        # check web newspaper exist
-        response = requests.get(remote_web_url, headers=headers)
-        if response.status_code == 403:
-            print("您选择的日期太久远，网站不提供")
-            exit(0)
-        if response.status_code == 404:
-            print("未找到指定日期的报纸，请尝试其他日期")
-            exit(0)
+def check_web_newspaper_exist(remote_web_url):
+    # check web newspaper exist
+
+    # print(remote_web_url)
+    response = requests.get(remote_web_url, headers=headers)
+
+    if response.status_code == 200:
+        return True
+
+    if response.status_code == 403:
+        print("您选择的日期太久远，网站不提供")
+        return False
+
+    if response.status_code == 404:
+        print("未找到指定日期的报纸，请尝试其他日期")
+        return False
+
+    print("未找到指定日期的报纸，请尝试其他日期")
+    return False
 
 
 def read_html_as_str(web_url):
@@ -109,14 +141,17 @@ def read_html_as_str(web_url):
 
 def download_file(file_url, save_folder):
     file_name = os.path.basename(file_url)
-    # response = requests.get(file_url, headers=headers)
-    # file = response.content
-    # with open(os.path.join(save_folder, file_name), "wb") as fn:
-    #     fn.write(file)
-    urllib.request.urlretrieve(file_url, os.path.join(save_folder, file_name))
+    try:
+        response = requests.get(file_url, headers=headers)
+        file = response.content
+        with open(os.path.join(save_folder, file_name), "wb") as fn:
+            fn.write(file)
+    except:
+        print("网络文件不存在，WebUrl:" + file_url)
 
 
 def merge_pdf(source_dir, filename_list, final_filename, target_dir):
+    creat_folder_if_not_exist(target_dir)
     print('合并{0}个PDF文件为单个PDF文件'.format(str(len(filename_list))))
 
     pdf_file_merger = PyPDF2.PdfFileMerger(strict=False)
--- peoples_daily_download.py
+++ peoples_daily_download.py
@@ -1,4 +1,5 @@
 # encoding = UTF-8
+import collections
 import os
 import re
 import requests
@@ -6,11 +7,12 @@ import requests
 import newspaper_helper as helper
 
 
-def download_newspaper(newspaper_cover_url, newspaper_download_url_format, temp_folder):
-    helper.creat_folder_if_not_exist(temp_folder)
+def download_newspaper(newspaper_cover_url, newspaper_download_url_format, date, temp_folder):
+    helper.init_or_clear_dir(temp_folder)
 
     print("下载中……")
-    print(newspaper_cover_url)
+
+    path_format_date = date[0:4] + "-" + date[4:6] + "/" + date[6:8]
 
     # 获取报纸页数
     response = requests.get(newspaper_cover_url, headers=helper.headers)
@@ -30,6 +32,35 @@ def download_newspaper(newspaper_cover_url, newspaper_download_url_format, temp_
     print("分片下载结束")
 
 
+def download_newspaper_by_date(date):
+    print("日期:" + date)
+    path_format_date = date[0:4] + "-" + date[4:6] + "/" + date[6:8]
+    newspaper_cover_url = newspaper_cover_url_format.format(path_format_date)
+    newspaper_pdf_filename = filename_prefix + date + ".pdf"
+    print("报纸封面链接:" + newspaper_cover_url)
+
+    if helper.check_local_newspaper_exist(helper.newspaper_saver_folder, newspaper_pdf_filename):
+        print("该日期已经下载过了!")
+        return
+
+    # 检测目标文件是否存在
+    if helper.check_web_newspaper_exist(newspaper_cover_url):
+        # 分片下载
+        download_newspaper(newspaper_cover_url, newspaper_download_url_format, date, helper.temp_folder)
+        # 获取排序后的分片文件名list
+        filename_list = sorted(os.listdir(helper.temp_folder),
+                               key=lambda x: os.path.getmtime(os.path.join(helper.temp_folder, x)))
+        print("本期总共{}版".format(str(len(filename_list))))
+        # 合并为整个pdf
+        helper.merge_pdf(helper.temp_folder, filename_list, newspaper_pdf_filename, helper.newspaper_saver_folder)
+        # 删除临时文件夹
+        helper.clear_dir(helper.temp_folder)
+        print(date + " 获取完成: " + newspaper_pdf_filename)
+        print("\n")
+    else:
+        print(date + "报纸不存在")
+
+
 if __name__ == '__main__':
 
     # newspaper config
@@ -38,40 +69,31 @@ if __name__ == '__main__':
     newspaper_cover_url_format = "http://paper.people.com.cn/rmrb/html/{}/nbs.D110000renmrb_01.htm"
     newspaper_download_url_format = "http://paper.people.com.cn/rmrb/images/{0}/{2}/rmrb{1}{2}.pdf"
 
+    print(newspaper_name)
+
     # 获取输入参数
     input_args = helper.get_input_arg()
 
-    # 日期
-    date = input_args.date
+    # 开始日期
+    start_date = input_args.start_date
+    # 结束日期
+    end_date = input_args.end_date
 
-    # 获取周报本周的出版日
-    date = helper.get_today_date() if (date is None or "none" == date) else date
+    print("--start_date:" + str(start_date))
+    print("--end_date:" + str(end_date))
 
-    path_format_date = date[0:4] + "-" + date[4:6] + "/" + date[6:8]
-    newspaper_cover_url = newspaper_cover_url_format.format(path_format_date)
-    newspaper_pdf_filename = filename_prefix + date + ".pdf"
-
-    print(newspaper_name)
-
-    # 日期 格式: 20220901
-    print("日期:" + date)
-
-    # 检测目标文件是否存在
-    helper.check_newspaper_exist(helper.newspaper_saver_folder, newspaper_cover_url, newspaper_pdf_filename)
-
-    # 分片下载
-    download_newspaper(newspaper_cover_url, newspaper_download_url_format, helper.temp_folder)
-
-    # 获取排序后的分片文件名list
-    filename_list = sorted(os.listdir(helper.temp_folder),
-                           key=lambda x: os.path.getmtime(os.path.join(helper.temp_folder, x)))
-    print("本期总共{}版".format(str(len(filename_list))))
-
-    # 合并为整个pdf
-    helper.merge_pdf(helper.temp_folder, filename_list, newspaper_pdf_filename, helper.newspaper_saver_folder)
-
-    # 删除临时文件夹
-    helper.clear_dir(helper.temp_folder)
-
-    print("获取完成: " + newspaper_pdf_filename)
-    print("\n")
+    if not helper.param_is_none(start_date):
+        # 开始日期不为空 下载开始日期到结束日期的所有报纸
+        end_date = helper.get_today_date() if helper.param_is_none(end_date) else end_date
+        print("final_start_date:" + start_date)
+        print("final_end_date:" + end_date)
+        date_list = helper.get_date_list(start_date, end_date)
+        for cur_date in date_list:
+            download_newspaper_by_date(cur_date)
+    else:
+        # 只获一天报纸
+        date = input_args.date
+        print("--date:" + str(date))
+        date = helper.get_today_date() if helper.param_is_none(date) else date
+        print("final_date:" + date)
+        download_newspaper_by_date(date)
