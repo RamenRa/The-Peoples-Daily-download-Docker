172cd8a46540413d5384a77e473d311d40878b81
RamenRa <53000259+RamenRa@users.noreply.github.com>
2024-05-17 20:05:58 +0800

    Update peoples_daily_download.py

--- peoples_daily_download.py
+++ peoples_daily_download.py
@@ -14,7 +14,7 @@ logging.basicConfig(
     format="%(asctime)s %(levelname)s:%(name)s: %(message)s",  
     level=logging.INFO,  
     datefmt="%Y-%m-%d %H:%M:%S",   
-    stream=sys.stderr,  
+    stream=sys.stdout,  
 )  
 logger = logging.getLogger("main")
 logging.getLogger("chardet.charsetprober").disabled = True
